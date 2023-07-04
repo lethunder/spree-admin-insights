9b8d39bf46fe011acca2f613cec9d5ecf884ff00
lethunder <didier.banlock@gmail.com>
2023-07-04 21:06:52 +0200

    Update product_decorator.rb

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,3 +1,10 @@
-Spree::Product.class_eval do
-  has_many :page_view_events, -> { viewed }, class_name: 'Spree::PageEvent', foreign_key: :target_id
+module Spree
+  module ProductDecorator
+    def self.prepended(base)
+      base.has_many :page_view_events, -> { viewed }, class_name: 'Spree::PageEvent', foreign_key: :target_id
+    end
+  end
 end
+
+
+::Spree::Product.prepend Spree::ProductDecorator if ::Spree::Product.included_modules.exclude?(Spree::ProductDecorator)
